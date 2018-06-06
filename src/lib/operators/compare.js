31db4a3fc0eb91ed67ccba0284b8eee35f1c6b60
andrewjones <aljones15@gmail.com>
2018-06-06 09:52:29 +0800

    using partial application so I can insert not as a different time

--- src/lib/operators/compare.js
+++ src/lib/operators/compare.js
@@ -1,3 +1,15 @@
+/**
+ * @function defaultNot
+ * @param {boolean}
+ * @return {boolean}
+ * @description defaultNot just returns a default boolean
+ */
+const function defaultNot (b) { return b; }
+
+/*
+ * @object compare
+ * @description just a simple organizer for all compare methods
+ */
 export const compare = {
   types: ['$eq', '$gt', '$gte', '$in', '$lt', '$lte', '$ne', '$nin'],
   parse: function(values, comparison) {
@@ -5,35 +17,44 @@ export const compare = {
     
   },
   $eq: function(values, comparison) {
+    const value = comparision.$eq;
     // https://docs.mongodb.com/manual/reference/operator/query/eq/
+    return ($not = defaultNot) => $not(values === value);
   },
   $gt: function(values, comparison) {
+    const value = comparison.$gt;
     // https://docs.mongodb.com/manual/reference/operator/query/gt/
-    return values.filter(v => v > comparison.$gt);
+    return ($not = defaultNot) => values.filter(v => $not(v > value));
   },
   $gte: function(values, comparison) {
+    const value = comparison.$gte;
     // https://docs.mongodb.com/manual/reference/operator/query/gte/
-    return values.filter(v => v >= comparison.$gte);
+    return ($not = defaultNot) => values.filter(v => $not(v >= value));
   },
   $lt: function(values, comparison) {
+    const value = comparison.$lt;
     // https://docs.mongodb.com/manual/reference/operator/query/lt/
-    return values.filter(v => v < comparison.$lt);
+    return ($not = defaultNot) => values.filter(v => $not(v < value));
   },
   $lte: function(values, comparison) {
+    const value = comparison.$lte;
     // https://docs.mongodb.com/manual/reference/operator/query/lte/
-    return values.filter(v => v <= comparison.$lte);
+    return ($not = defaultNot) => values.filter(v => $not(v <= value));
   }
   $in: function(values, comparison) {
+    const value = comparison.$in;
     // https://docs.mongodb.com/manual/reference/operator/query/in/
-    return values.filter(v => comparison.$in.includes(v));
+    return ($not = defaultNot) => values.filter(v => $not(value.includes(v)));
   }
   $ne: function(values, comparison) {
+    const value = comparision.$ne;
     // https://docs.mongodb.com/manual/reference/operator/query/ne/
-    return values.filter(v => v != comparison.$ne);
+    return ($not = defaultNot) => values.filter(v => $not(v != value));
   }
   $nin: function(values, comparison) {
+    const value = comparison.$nin;
     // https://docs.mongodb.com/manual/reference/operator/query/nin/
-    return values.filter(v => !comparison.$nin.includes(v));
+    return ($not = defaultNot) => values.filter(v => $not(!value.includes(v)));
   }
 }
 
