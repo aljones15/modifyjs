f94ee6b381b020078132a3ccf828253c4c3288ef
andrewjones <aljones15@gmail.com>
2018-06-08 10:08:14 +0800

    wrote logicals

--- src/lib/operators/element.js
+++ src/lib/operators/element.js
@@ -1,9 +1,11 @@
 export const element = {
-  $exists: function() {
+  $exists: function(values, comparison) {
+     const value = comparision.$exists;
      // matches documents with the specified field
      // https://docs.mongodb.com/manual/reference/operator/query/exists/
+     return values.filter(v => v === value);
    },
-   $type: function() {
+   $type: function(values, comparison) {
      // selects docs if specified type i.e. int, string, etc.
      // https://docs.mongodb.com/manual/reference/operator/query/type/
    }
--- src/lib/operators/logical.js
+++ src/lib/operators/logical.js
@@ -1,19 +1,23 @@
 export const logicals {
   types: ['$not', '$or', '$and', '$nor'],
-  $not: function() {
+  $not: function(b) {
     // just inverts the condition
     // https://docs.mongodb.com/manual/reference/operator/query/not/
+    return !b;
   },
-  $or: function() {
+  $or: function(all, a, b) {
     // not XOR
     // https://docs.mongodb.com/manual/reference/operator/query/or/
+    return all.filter(v => b.includes(v) || a.includes(v));
   },
-  $and: function() {
+  $and: function(all, a, b) {
     // returns elements that do match both conditions
     // https://docs.mongodb.com/manual/reference/operator/query/and/
+    return all.filter(v => a.includes(v) && b.includes(v));
   },
-  $nor: function() {
+  $nor: function(all, a, b) {
     // returns elements that do not match both of the conditions
     // https://docs.mongodb.com/manual/reference/operator/query/nor/
+    return all.filter(v => !a.includes(v) && !b.includes(v));
   }
 }
